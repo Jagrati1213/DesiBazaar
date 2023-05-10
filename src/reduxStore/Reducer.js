a3300bedf5d86914ac5e8694e381d24b52adf896
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-10 18:37:44 +0530

    jg: add redux

---
+++ src/reduxStore/Reducer.js
@@ -0,0 +1,7 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+const initialState = {
+    products: [],
+}
+export const productReducre = createReducer(initialState, {});
+
---
+++ src/reduxStore/Store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { productReducre } from './Reducer';
+
+export const store = configureStore({
+    reducer: {
+        product: productReducre
+    }
+});
