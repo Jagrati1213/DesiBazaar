61ec42c769112e197e341d67eb8a6c465538b4c3
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-11 17:10:14 +0530

    fetch real data

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,47 +1,77 @@
-import React from 'react';
-import { Col, Row,Image,StarFilled } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Col, Row,Image } from 'antd';
+import { StarFilled } from "@ant-design/icons";
+import { BsBagHeartFill } from "react-icons/bs";
+import { IconContext } from 'react-icons';
+import { useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 function SingleProduct() {
+ 
+  const [singlepr, setSinglePr] = useState(null);
+  const [rate, setRate] = useState(0);
 
+  // for get productId 
+  const params = useParams();
+  
+   useEffect(()=>{
+
+    const fetchSingleProduct = async()=>{
+
+       await axios.get(`https://fakestoreapi.com/products/${params.producId}`)
+        .then(res=> {
+          setSinglePr(res.data)
+          setRate(res.data?.rating?.rate)
+        })
+        .catch((err)=> console.log(err))
+    }
+    fetchSingleProduct();
+   },[params]);
 
   return (
     <main className='mt-[90px] w-full min-h-screen py-[4rem]'>
-    <Row className=' h-auto mx-[10rem]'>
-      <Col span={12} className='border flex justify-center items-center h-3/6 p-4'>
-      <Image.PreviewGroup
-          preview={{
-            onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
-          }}
-        >
-          <Image width={500} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" className=' object-cover w-full' />
-      </Image.PreviewGroup>
-      </Col>
-      <Col span={12} className='border p-8'>
-          
-          <h1 className='text-3xl text-slate font-bold uppercase'>
-            TITLE
-          </h1>
-          <p className='text-lg text-gray-500 break-words my-4'> 
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque laborum eius sed.
-          </p>
-          <p className='text-lg text-black break-words my-4 font-semibold'> 
-              Category
-          </p>
-          <p className='text-lg text-black break-words my-4 font-bold'> 
-              ₹ 2344
-          </p>
-          <div className='raiting py-12 bg-red-300'>
-            <p>hello</p>
-            {[Array(3).map((_,index)=>{
-                 return<StarFilled key={index} />
-              })
-              ]}
-          </div>
-          
+      {
+        singlepr !== null?(
+          <Row className='h-auto mx-[10rem]'>
 
-      </Col>
-    </Row>
+            {/* SingleProduct Image */}
+            <Col span={12} className='border flex justify-center items-center p-4 overflow-hidden'>
+            <Image.PreviewGroup
+                preview={{
+                  onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+                }}>
+                <Image width={500} src={singlepr.image} className='object-cover w-full'/>
+            </Image.PreviewGroup>
+            </Col>
+
+            {/* SingleProduct Details */}
+            <Col span={12} className='border p-8'>
+                
+                <h1 className='text-3xl text-slate font-bold uppercase'>{singlepr.title}</h1>
+                <p className='text-lg text-gray-500 break-words my-4'> {singlepr.description}</p>
+                <p className='text-lg text-black break-words my-4 font-semibold'> Category: {singlepr.category}</p>
+                <p className='text-lg text-black break-words my-4 font-bold'> <i className='font-normal'>Price :</i> ₹ {singlepr.price}</p>
+                <div className='raiting py-7 text-yellow-500 flex items-center text-3xl'>      
+                  {Array(Math.ceil(rate)).fill(null).map((_,i)=>{
+                      return <StarFilled className='mx-1' key={i}/>
+
+                    })
+                  }
+                </div>
+                <button className='font-bold rounded text-whiteSmoke bg-slate hover:bg-black py-3 px-10 text-base my-10 flex items-center'>
+                  <IconContext.Provider value={{size:'18px'}}><BsBagHeartFill className='mr-2'/> </IconContext.Provider>
+                  <span>Add to bag</span>
+                </button>
+                
+
+            </Col>
+         </Row>
+        ):(
+          <h1> Somthing is wrong</h1>
+        )
+      }
+
     </main>
   )
 }
