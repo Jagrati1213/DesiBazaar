7ad472547a74aa9062e08290b141bf4bee221216
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-11 19:34:57 +0530

    use slice

--- src/reduxStore/Reducer.js
+++ src/reduxStore/Reducer.js
@@ -1,7 +1,19 @@
-import { createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     products: [],
 }
-export const productReducre = createReducer(initialState, {});
+export const productReducre = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addItems: (state, acton) => {
+            state.products.push(acton.payload)
+        }
+    }
+})
+export const { addItems } = productReducre.actions;
+export default productReducre.reducer
+
+
 
