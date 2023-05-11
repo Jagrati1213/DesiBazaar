13788ed4ad246f70978b8ec8f1899f4e4c192c07
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-11 17:11:18 +0530

    used Link to redirect singleproduct

--- src/components/lmitProduct/LimitProduct.jsx
+++ src/components/lmitProduct/LimitProduct.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Card,Col,Row } from 'antd';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const { Meta } = Card;
 
@@ -17,10 +17,10 @@ function LimitProduct({baseurl,title}) {
         }catch(err){
          console.log(err);
         }
-         
     }
     fetchingData(); 
  },[]);
+
   return (
     <main className='p-4 md:p-10 w-full my-10 site-card-wrapper'>
         <h1 className='w-full text-center font-bold text-3xl text-slate my-10'> {title}</h1>
@@ -30,23 +30,23 @@ function LimitProduct({baseurl,title}) {
                 limitProduct ?(
                         limitProduct.map(i=>{
                             return <Col className="gutter-row" span={6} key={i.id} style={{cursor:'pointer'}}> 
-                                    <Link to={`/product/${i.id}`}>
+                                   <Link to={`/product/${i.id}`}>
                                         <Card style={{overflow:'hidden', marginBottom:'2rem'}} 
                                             cover={
                                                 <div style={{ overflow: "hidden", height: "250px" , width:'100%'}}>
                                                 <img
-                                                  alt="item"
-                                                  style={{width:'100%',objectFit:'contain',height:'100%',padding:'12px' }}
-                                                  src={i.image}
+                                                alt="item"
+                                                style={{width:'100%',objectFit:'contain',height:'100%',padding:'12px' }}
+                                                src={i.image}
                                                 />
-                                              </div>
+                                            </div>
                                             }
                                             actions={[]}
                                             >
                                         <Meta title={i.title} description={`₹${i.price}`}/>
                                         </Card>
-                                    </Link>
-                                </Col>
+                                   </Link>
+                                 </Col>
                         })
                 ):(
                   <h1>Somthing error</h1>                    
