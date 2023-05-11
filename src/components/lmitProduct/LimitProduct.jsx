44bba429300adeb848d9a78f397a9b3aae0212f5
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-11 13:40:35 +0530

    limit product

--- src/components/lmitProduct/LimitProduct.jsx
+++ src/components/lmitProduct/LimitProduct.jsx
@@ -1,51 +1,55 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Card,Col,Row,Skeleton } from 'antd';
+import { Card,Col,Row } from 'antd';
+import { Link } from 'react-router-dom';
 
 const { Meta } = Card;
 
-function LimitProduct() {
+function LimitProduct({baseurl,title}) {
 
     const [limitProduct, setLimitProduct] = useState(null);
 
  useEffect(()=>{
     const fetchingData = async()=>{
         try{
-            const { data } = await axios.get(`https://fakestoreapi.com/products/category/jewelery`);
+            const { data } = await axios.get(baseurl);
             setLimitProduct(data);
-            console.log(data);
         }catch(err){
          console.log(err);
         }
          
     }
-    fetchingData();
+    fetchingData(); 
  },[]);
   return (
-    <main className='px-4 md:px-10 w-full my-10 site-card-wrapper'>
-        <h1 className='w-full text-center font-bold text-3xl text-slate my-10'> Jewelery</h1>
-        <Row gutter={{  xs: 8, sm: 16, md: 24, lg: 32 }} justify="space-evenly">
+    <main className='p-4 md:p-10 w-full my-10 site-card-wrapper'>
+        <h1 className='w-full text-center font-bold text-3xl text-slate my-10'> {title}</h1>
+        <Row gutter={{  xs: 8, sm: 16, md: 24, lg: 32 }} justify="space-center">
 
             {
                 limitProduct ?(
                         limitProduct.map(i=>{
-                            return <Col className="gutter-row" span={5} key={i.id}> 
-                                        <Card style={{width: 300, height:'400px', overflow:'hidden', marginBottom:'2rem'}} 
+                            return <Col className="gutter-row" span={6} key={i.id} style={{cursor:'pointer'}}> 
+                                    <Link to={`/product/${i.id}`}>
+                                        <Card style={{overflow:'hidden', marginBottom:'2rem'}} 
                                             cover={
-                                                <div style={{ overflow: "hidden", height: "200px" , width:'100%'}}>
+                                                <div style={{ overflow: "hidden", height: "250px" , width:'100%'}}>
                                                 <img
-                                                  alt="example"
-                                                  style={{width:'100%',objectFit:'contain',height:'100%' }}
+                                                  alt="item"
+                                                  style={{width:'100%',objectFit:'contain',height:'100%',padding:'12px' }}
                                                   src={i.image}
                                                 />
                                               </div>
-                                            }>
-                                        <Meta title={i.title} description={i.description} />
+                                            }
+                                            actions={[]}
+                                            >
+                                        <Meta title={i.title} description={`₹${i.price}`}/>
                                         </Card>
-                                    </Col>
+                                    </Link>
+                                </Col>
                         })
                 ):(
-                    <Skeleton avatar paragraph={{rows: 4,}}/>
+                  <h1>Somthing error</h1>                    
                 )
             }
         </Row>
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Col, Row,Image,StarFilled } from 'antd';
+
 
 function SingleProduct() {
+
+
   return (
-    <div>SingleProduct</div>
+    <main className='mt-[90px] w-full min-h-screen py-[4rem]'>
+    <Row className=' h-auto mx-[10rem]'>
+      <Col span={12} className='border flex justify-center items-center h-3/6 p-4'>
+      <Image.PreviewGroup
+          preview={{
+            onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
+          }}
+        >
+          <Image width={500} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" className=' object-cover w-full' />
+      </Image.PreviewGroup>
+      </Col>
+      <Col span={12} className='border p-8'>
+          
+          <h1 className='text-3xl text-slate font-bold uppercase'>
+            TITLE
+          </h1>
+          <p className='text-lg text-gray-500 break-words my-4'> 
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque laborum eius sed.
+          </p>
+          <p className='text-lg text-black break-words my-4 font-semibold'> 
+              Category
+          </p>
+          <p className='text-lg text-black break-words my-4 font-bold'> 
+              ₹ 2344
+          </p>
+          <div className='raiting py-12 bg-red-300'>
+            <p>hello</p>
+            {[Array(3).map((_,index)=>{
+                 return<StarFilled key={index} />
+              })
+              ]}
+          </div>
+          
+
+      </Col>
+    </Row>
+    </main>
   )
 }
 
