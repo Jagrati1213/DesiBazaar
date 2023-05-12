e1e0d1c3c07e4506c40e6783c746d19bae1d024d
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-12 16:25:38 +0530

    costomize code

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -3,10 +3,11 @@ import { Col, Row,Image } from 'antd';
 import { StarFilled } from "@ant-design/icons";
 import { BsBagHeartFill } from "react-icons/bs";
 import { IconContext } from 'react-icons';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addItems } from '../reduxStore/Reducer';
+import { toast } from 'react-hot-toast';
 
 
 function SingleProduct() {
@@ -20,22 +21,23 @@ function SingleProduct() {
   // for using additem
   const dispatch = useDispatch();
 
+  // fetching single product
+  const fetchSingleProduct = async()=>{
+
+    await axios.get(`https://fakestoreapi.com/products/${params.producId}`)
+     .then(res=> {
+       setSinglePr(res.data)
+       setRate(res.data?.rating?.rate)
+     })
+     .catch((err)=> console.log(err))
+ }
    useEffect(()=>{
-
-    const fetchSingleProduct = async()=>{
-
-       await axios.get(`https://fakestoreapi.com/products/${params.producId}`)
-        .then(res=> {
-          setSinglePr(res.data)
-          setRate(res.data?.rating?.rate)
-        })
-        .catch((err)=> console.log(err))
-    }
     fetchSingleProduct();
    },[params]);
 
   //  set product to addItem
    const addToCart = (singlepr)=>{
+     toast.success('Add to Cart')
      dispatch(addItems(singlepr))
    }
 
@@ -59,9 +61,13 @@ function SingleProduct() {
             <Col span={12} className='p-8'>
                 
                 <h1 className='text-3xl text-slate font-bold uppercase'>{singlepr.title}</h1>
+
                 <p className='text-lg text-gray-500 break-words my-4'> {singlepr.description}</p>
+
                 <p className='text-lg text-black break-words my-4 font-semibold'> Category: {singlepr.category}</p>
+
                 <p className='text-lg text-black break-words my-4 font-bold'> <i className='font-normal'>Price :</i> ₹ {singlepr.price}</p>
+                
                 <div className='raiting py-7 text-yellow-500 flex items-center text-3xl'>      
                   {Array(Math.ceil(rate)).fill(null).map((_,i)=>{
                       return <StarFilled className='mx-1' key={i}/>
@@ -69,15 +75,14 @@ function SingleProduct() {
                     })
                   }
                 </div>
+                
                 <button 
-                onClick={(singlepr)=>{
-                  addToCart(singlepr)}} 
+                onClick={()=>{ addToCart({singlepr}) }} 
                   className='font-bold rounded text-whiteSmoke bg-slate hover:bg-black py-3 px-10 text-base my-10 flex items-center'>
                   <IconContext.Provider value={{size:'18px'}}><BsBagHeartFill className='mr-2'/> </IconContext.Provider>
                   <span>Add to bag</span>
                 </button>
                 
-
             </Col>
          </Row>
         ):(
