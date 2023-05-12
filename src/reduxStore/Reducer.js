15c0dfc10166f89d9796a14080858210ea241a1a
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-12 16:26:22 +0530

    additem function

--- src/reduxStore/Reducer.js
+++ src/reduxStore/Reducer.js
@@ -2,14 +2,29 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     products: [],
+    quantity: 1,
 }
 export const productReducre = createSlice({
     name: "cart",
     initialState,
     reducers: {
         addItems: (state, action) => {
-            console.log(action.payload);
-            state.products.push(action.payload)
+            const item = action.payload;
+            const isItemExit = state.products.find((i) => i.id === item.id);
+
+            if (isItemExit) {
+                // already exit in cart
+                state.products.forEach((i) => {
+                    if (i.id === item.id) {
+
+                    }
+                })
+
+            }
+            else {
+                // Added to Cart;
+                state.products.push(item);
+            }
         }
     }
 })
