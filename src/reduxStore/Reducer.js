238f1bbf71761afd9932cf42d21a8f3f8a3c0932
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-14 11:48:31 +0530

    add quantity

--- src/reduxStore/Reducer.js
+++ src/reduxStore/Reducer.js
@@ -2,33 +2,56 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     products: [],
-    quantity: 1,
 }
 export const productReducre = createSlice({
     name: "cart",
     initialState,
     reducers: {
+
         addItems: (state, action) => {
             const item = action.payload;
-            const isItemExit = state.products.find((i) => i.id === item.id);
-
+            const isItemExit = state.products.find((i) => i.item.id === item.id);
             if (isItemExit) {
                 // already exit in cart
-                state.products.forEach((i) => {
-                    if (i.id === item.id) {
-
+                state.products.forEach((ele) => {
+                    if (ele.item.id === item.id) {
+                        ele.quantity += 1;
                     }
                 })
-
             }
             else {
                 // Added to Cart;
-                state.products.push(item);
+                state.products.push({ item, quantity: 1 });
             }
+        },
+
+        decrementItems: (state, action) => {
+            const itemId = action.payload;
+
+            state.products.forEach((ele) => {
+                if (ele.item.id === itemId) {
+                    return ele.quantity -= 1;
+                } else {
+                    return ele.quantity = ele.quantity
+                }
+            })
+        },
+
+        incrementItems: (state, action) => {
+            const itemId = action.payload;
+
+            state.products.forEach((ele) => {
+                if (ele.item.id === itemId) {
+                    return ele.quantity += 1;
+                } else {
+                    return ele.quantity = ele.quantity
+                }
+            })
         }
+
     }
 })
-export const { addItems } = productReducre.actions;
+export const { addItems, decrementItems, incrementItems } = productReducre.actions;
 export default productReducre.reducer
 
 
