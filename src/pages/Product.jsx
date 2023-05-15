a9e3d837f54fe97d7c40730cd4a6e0ba68c55969
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-15 17:44:58 +0530

    remove unused parts

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { Col, Row, Card } from 'antd';
-import { Link } from 'react-router-dom';
 import LimitProduct from '../components/lmitProduct/LimitProduct';
 
-const { Meta } = Card;
-
-function Product() {
-    
+function Product() {  
   return (
     <main className='mt-[90px]'>
       <LimitProduct baseurl={'https://fakestoreapi.com/products'} title={'All products'}/>
