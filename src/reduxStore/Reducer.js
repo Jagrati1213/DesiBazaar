7c4d6b0b87d6ab6bcc686c4cd9e866994154462c
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-15 11:54:55 +0530

    add va;ues on initial state

--- src/reduxStore/Reducer.js
+++ src/reduxStore/Reducer.js
@@ -2,6 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     products: [],
+    total: 0,
+    subTotal: 0,
+    delivery: 0
 }
 export const productReducre = createSlice({
     name: "cart",
@@ -29,7 +32,7 @@ export const productReducre = createSlice({
             const itemId = action.payload;
 
             state.products.forEach((ele) => {
-                if (ele.item.id === itemId) {
+                if (ele.item.id === itemId && ele.quantity > 0) {
                     return ele.quantity -= 1;
                 } else {
                     return ele.quantity = ele.quantity
@@ -39,19 +42,30 @@ export const productReducre = createSlice({
 
         incrementItems: (state, action) => {
             const itemId = action.payload;
-
             state.products.forEach((ele) => {
                 if (ele.item.id === itemId) {
                     return ele.quantity += 1;
+
                 } else {
                     return ele.quantity = ele.quantity
                 }
             })
+        },
+
+        calculatePrice: (state) => {
+            let sum = 0;
+            state.products.forEach((i) =>
+                (sum += Number(i.item.price) * Number(i.quantity))
+            );
+            state.subTotal = sum;
+            state.delivery = state.subTotal > 1000 ? 0 : 200;
+            state.total = state.subTotal + state.delivery;
         }
+
 
     }
 })
-export const { addItems, decrementItems, incrementItems } = productReducre.actions;
+export const { addItems, decrementItems, incrementItems, calculatePrice } = productReducre.actions;
 export default productReducre.reducer
 
 
