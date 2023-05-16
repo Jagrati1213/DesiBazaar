361b748b11fc09a536645d280d48041ddf10980f
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-16 17:28:43 +0530

    add methods

--- src/reduxStore/AuthReducer.js
+++ src/reduxStore/AuthReducer.js
@@ -2,27 +2,42 @@ import { createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-hot-toast';
 
 const initialState = {
-    userDetails: []
+    userDetails: [],
+    isUser: false,
 }
 const userDetailsSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
+
         logIn: (state, action) => {
             const user = action.payload;
+            const IsLoggin = state.userDetails.find((i) => i.username === user.username && i.password === user.password);
+
+            if (IsLoggin) {
+                toast.success(`Welcome Back ${user.username}`);
+                state.isUser = true;
+            } else {
+                toast.error('Oops!! Create account first');
+                state.isUser = false;
+            }
         },
+
         sigIn: (state, action) => {
 
             const user = action.payload;
-            const IsLoggin = state.userDetails.find((i) => i.username === user.username);
+            const IsLoggin = state.userDetails.find((i) => i.username === user.username && i.password === user.password);
             if (IsLoggin) {
-                console.log('exit');
-                toast.error('User already exit')
+                toast.error('User already exit');
+                state.isUser = false;
+
             } else {
                 toast.success(`Welcom ${user.username}`);
                 state.userDetails.push(user);
+                state.isUser = true;
             }
         },
+
     }
 });
 
