cbf4712f6d1e35b80a5781a3f7aff5df91ce67a8
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-17 18:25:52 +0530

    set profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,23 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { logOut } from '../reduxStore/AuthReducer';
+
+function Profile() {
+    
+    const dispatch = useDispatch();
+    const handleLogout = ()=>{
+         dispatch(logOut());
+    };
+    useEffect(()=>{},[dispatch]);
+
+  return (
+        <main className='w-full mt-[90px] min-h-screen flex items-center flex-col'>
+            <div className='flex justify-evenly flex-col items-center border-2 border-zinc-400 rounded m-5 py-6 w-[900px]'>
+                <h3 className='text-3xl text-slate font-semibold text-center  uppercase'>Welcome,</h3>
+                <button onClick={handleLogout} className='text-whiteSmoke bg-slate py-2 px-10 rounded mt-8 hover:bg-black'>Log Out</button>
+            </div>
+        </main>
+  )
+}
+
+export default Profile;
