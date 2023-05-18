9f1f89e5ba6e1c99108dd5d7dc1453be484138f8
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-18 19:01:25 +0530

    add dynamic carts, but there is bug

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import { Col,Row, } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import { calculatePrice, decrementItems, incrementItems } from '../reduxStore/ProductReducer';
@@ -7,10 +7,14 @@ import { calculatePrice, decrementItems, incrementItems } from '../reduxStore/Pr
 
 function Cart() {
 
-  // **** Get productReducre -> store *** //
-  const {products, total, subTotal, delivery} = useSelector(state => state.product);
+  //_____ Get productReducre -> store 
+  const { total, subTotal, delivery} = useSelector(state => state.product);
 
-  // **** Get Methods -> productReducre ***//
+  const { userDetails } = useSelector(state => state.user);
+  const currentUserIndex = userDetails.indexOf(userDetails.find((i) => i.isUser === true));
+
+
+  //_____ Get Methods -> productReducre 
   const dispatch = useDispatch();
 
   // **** Decrement the Items **** //
@@ -19,44 +23,46 @@ function Cart() {
       dispatch(calculatePrice());
   }
 
-  // **** Increment the Items **** //
+  //_____ Increment the Items 
   const increment = (itemId) =>{
    dispatch(incrementItems(itemId));
    dispatch(calculatePrice());
 }
 
+useEffect(()=>{},[dispatch]);
+
   return (
-    <main className='mt-[90px] p-10'>
+    <main className='mt-[90px] py-10'>
       <Row className='w-full mx-auto'>
-        { products.length > 0?
+        { (userDetails[currentUserIndex].userCart.length > 0 && userDetails[currentUserIndex].isUser === true)?
           (
           <>
             {/* Cart Items */}
-            <Col span={16} className='p-8 border-r-2'>
+            <Col xs={{span: 24 }} xl={{span: 16}} className='p-8 border-r-2'>
               <ul className=' overflow-y-scroll'>
                  {
-                  products.map((item,index)=>{
+                  userDetails[currentUserIndex].userCart.map((ele,index)=>{
                     return(
                     <li className='flex my-10 justify-between' key={index}>
 
                       <div  className='flex'>
                           <div className='p-4 w-[200px] '>
-                            <img src={item.item?.image} alt="product " className='w-full object-cover'/>
+                            <img src={ele.item?.image} alt="product " className='w-full object-cover'/>
                           </div>
 
                           <div className='p-4'>
-                            <h3 className='text-lg text-slate font-bold'>{item.item?.title}</h3>
-                            <p className='text-base text-gray-600 whitespace-nowrap overflow-hidden text-ellipsis w-[200px]'>{item.item?.description}</p>
+                            <h3 className='text-lg text-slate font-bold'>{ele.item?.title}</h3>
+                            <p className='text-base text-gray-600 whitespace-nowrap overflow-hidden text-ellipsis w-[200px]'>{ele.item?.description}</p>
                             <div className='flex items-center my-2 text-zinc-800 font-medium'> 
-                              <span className='mr-2'>Price : ₹{Math.ceil(item.item?.price)}</span>
+                              <span className='mr-2'>Price : ₹{Math.ceil(ele.item?.price)}</span>
                             </div>
                             <p className=' text-crimson hover:underline cursor-pointer text-base'>Delete the item</p>
                           </div>
                       </div>
                        <div className='p-4 flex justify-center items-center text-lg font-medium'>
-                           <button className='py-1 px-4 rounded text-whiteSmoke bg-slate' onClick={()=> decrement(item.item?.id)}>-</button>
-                           <span className='text-base text-center mx-4'>{item?.quantity}</span>
-                           <button className='py-1 px-4 rounded text-whiteSmoke bg-slate' onClick={()=> increment(item.item?.id)}>+</button>
+                           <button className='py-1 px-4 rounded text-whiteSmoke bg-slate' onClick={()=> decrement(ele.item?.id)}>-</button>
+                           <span className='text-base text-center mx-4'>{ele?.quantity}</span>
+                           <button className='py-1 px-4 rounded text-whiteSmoke bg-slate' onClick={()=> increment(ele.item?.id)}>+</button>
                       </div>
                   </li>)
                   })
@@ -65,9 +71,9 @@ function Cart() {
             </Col>
 
             {/* Cart items total */}
-            <Col span={8} className='p-8 flex justify-center'>
-                    <aside className='rounded'>
-                        <div className=" bg-zinc-200 border p-6 w-[500px] text-[18px]">
+            <Col xs={{span: 24 }} xl={{span: 8}} className='p-8 flex justify-center '>
+                    <aside className='rounded xl:w-[500px] w-[-webkit-fill-available]'>
+                        <div className=" bg-zinc-200 border p-6 w-auto text-[18px]">
 
                           <div className="item_total mt-4 overflow-hidden py-2 flex justify-between">
                             <div className="itemtotal-title">Item Total</div>
@@ -97,7 +103,7 @@ function Cart() {
           <Col className='flex justify-center items-center border-2 py-20 flex-col' span={24}>
             <p className='text-slate text-center font-semibold italic text-2xl'> Your Bag is too Light</p>
             <Link to='/'>
-               <button className='py-4 px-5 text-whiteSmoke bg-slate hover:bg-black mt-10 rounded'> Continue to Shopping</button>
+               <button className='py-4 px-5 text-whiteSmoke border-b-2 hover:bg-black mt-10 rounded'> Continue to Shopping</button>
             </Link>
           </Col>
         )
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,12 +3,17 @@ import { Card,Col,Row } from 'antd';
 import {Link} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItems, calculatePrice } from '../reduxStore/ProductReducer';
+import { userCart } from '../reduxStore/AuthReducer';
 
 const { Meta } = Card;
 
 function Wishlist() {
 
     const {listArr} = useSelector((state)=> state.product);
+
+    const { userDetails } = useSelector(state => state.user);
+    const currentUserIndex = userDetails.indexOf(userDetails.find((i) => i.isUser === true));
+    console.log(userDetails[currentUserIndex].userWish,'userWish', currentUserIndex);
 
     //_____ For Calling AddToCart 
     const dispatch = useDispatch();
@@ -21,6 +26,7 @@ function Wishlist() {
         const getItem = listArr.find((k)=> k.item.id === id);
         if(getItem){
             dispatch(addItems(i.item));
+            dispatch(userCart(i.item));
             dispatch(calculatePrice());
         }
       })
@@ -32,8 +38,9 @@ function Wishlist() {
         <Row gutter={{ xs: 8, sm: 16, lg: 18 }} justify="space-center" className='w-100 gap-6'>
 
             {
-                listArr.length ?(
-                    listArr.map((i)=>{
+               (userDetails[currentUserIndex].userWish.length > 0 && userDetails[currentUserIndex].isUser === true)?
+                  (userDetails[currentUserIndex].userWish.map((i)=>{
+                    console.log(i);
                         return <Col className="gutter-row bg-white rounded py-4 border-2" 
                                     xs={{span: 24, }} lg={{span: 6}} md={{span: 8}} 
                                     key={i.item.id} style={{cursor:'pointer'}}> 
@@ -55,14 +62,14 @@ function Wishlist() {
                                    <button className='w-full py-4 px-6 text-slate border-t-2 uppercase text-md font-bold' onClick={()=> getItemId(i.item.id)}>move to bag</button>
                                  </Col>
                         })
-                ):(
-                  <Col className='flex justify-center items-center border-2 py-20 flex-col' span={24}>
-                  <p className='text-slate text-center font-semibold italic text-2xl'> Nothing you store 🫣</p>
-                  <Link to='/'>
-                     <button className='py-4 px-5 text-whiteSmoke bg-slate hover:bg-black mt-10 rounded'> Continue to Shopping</button>
-                  </Link>
-                </Col>                   
-                )
+                  ):(
+                    <Col className='flex justify-center items-center border-2 py-20 flex-col' span={24}>
+                    <p className='text-slate text-center font-semibold italic text-2xl'> Nothing you store 🫣</p>
+                    <Link to='/'>
+                      <button className='py-4 px-5 text-whiteSmoke bg-slate hover:bg-black mt-10 rounded'> Continue to Shopping</button>
+                    </Link>
+                  </Col>                   
+                  )
             }
         </Row>
     </main>
