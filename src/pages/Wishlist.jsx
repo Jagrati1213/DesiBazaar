5fc5598c10dae7dd51a0e84b4b86e068d3c2bae2
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-19 17:14:31 +0530

    set move bag & delete

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,7 +3,8 @@ import { Card,Col,Row } from 'antd';
 import {Link} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 // import { addItems, calculatePrice } from '../reduxStore/ProductReducer';
-import { calculatePrice, userCartList } from '../reduxStore/AuthReducer';
+import { calculatePrice, removeListItem, userCartList } from '../reduxStore/AuthReducer';
+import { CloseCircleFilled } from '@ant-design/icons';
 
 const { Meta } = Card;
 
@@ -31,6 +32,11 @@ function Wishlist() {
         })
      },[dispatch])
 
+    //_____ Delete wishlist item
+    const deleteList = useCallback((itemId)=>{
+          dispatch(removeListItem(itemId));
+    },[dispatch]);
+
 
   return (
     <main className='mt-[90px] w-full min-h-screen p-10 overflow-hidden'>
@@ -40,19 +46,25 @@ function Wishlist() {
             (
                userDetails[currentUserIndex].userWish.map((ele)=>{
                 return(
-                  <Col className="gutter-row bg-white rounded py-4 border-2"  xs={{span: 24, }} lg={{span: 6}} md={{span: 8}} style={{cursor:'pointer'}}> 
-                      <Link to={`/product/${ele.cartItem.id}`}>
-                        <Card style={{overflow:'hidden', marginBottom:'2rem', textAlign:'center', boxShadow:'none', border:'none'}} 
-                              cover={
-                            <div style={{ overflow: "hidden", height: "250px" , width:'100%'}}>
-                                <img alt="item"
-                                      style={{width:'100%',objectFit:'contain',height:'100%',padding:'12px' }}
-                                      src={ele.cartItem.image}
-                                  />
-                              </div>
-                             }>
-                            <Meta title={'hello'} description={`₹${Math.ceil(ele.cartItem.price)}`}/>
-                          </Card>
+                  <Col key={ele.cartItem?.id} className="gutter-row bg-white rounded py-4 border-2 relative"  xs={{span: 24, }} lg={{span: 6}} md={{span: 8}} style={{cursor:'pointer'}}> 
+
+                        <span className='text-zinc-400 hover:text-slate absolute right-2 top-0 z-30 text-2xl' 
+                              onClick={()=> deleteList(ele.cartItem?.id)}>
+                                <CloseCircleFilled />
+                        </span>
+                        <Link to={`/product/${ele.cartItem.id}`}>
+                          <Card style={{overflow:'hidden', marginBottom:'2rem', textAlign:'center', boxShadow:'none', border:'none'}} 
+                                cover={
+                              <div style={{ overflow: "hidden", height: "250px" , width:'100%'}}>
+                                  <img alt="item"
+                                        style={{width:'100%',objectFit:'contain',height:'100%',padding:'12px' }}
+                                        src={ele.cartItem.image}
+                                    />
+                                </div>
+                              }>
+                              <Meta title={'hello'} description={`₹${Math.ceil(ele.cartItem.price)}`}/>
+                            </Card>
+
                         </Link>
                        <button className='w-full py-4 px-6 text-slate border-t-2 uppercase text-md font-bold' onClick={()=> getItemId(ele.cartItem.id)}>
                                   move to bag
@@ -62,8 +74,8 @@ function Wishlist() {
                })
             ):
             (
-              <Col className='flex justify-center items-center border-2 py-20 flex-col' span={24}>
-                  <p className='text-slate text-center font-semibold italic text-2xl'> Nothing you store 🫣</p>
+              <Col className='flex justify-center items-center border-2 py-20 flex-col mx-auto' span={16}>
+                <p className='text-slate text-center font-semibold italic text-2xl'> Nothing you store 🫣</p>
                   <Link to='/'>
                     <button className='py-4 px-5 text-slate border-b-2 border-slate hover:text-black mt-10'> Continue to Shopping</button>
                   </Link>
