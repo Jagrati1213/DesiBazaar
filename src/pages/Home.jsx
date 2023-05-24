74950a10d167ad76ff04311a180affb2d61ea62a
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-24 13:42:11 +0530

    ss: vercel check approved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <main className='h-full min-h-screen w-full'>
           <Craousel/>
-          <LimitProduct baseurl={'https://fakestoreapi.com/products/category/jewelery'} title={"hello"}/>
+          <LimitProduct baseurl={'https://fakestoreapi.com/products/category/jewelery'} title={"Jewelery"}/>
           <LimitProduct baseurl={'https://fakestoreapi.com/products/category/electronics'} title={"Electronics"}/>
     </main>
   )
