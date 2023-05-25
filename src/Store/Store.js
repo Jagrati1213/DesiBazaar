5ba38ab2313c8394cded9e96da4bbd8c6876a114
Jagrati Gupta <123jagratigupta@gmail.com>
2023-05-25 13:02:11 +0530

    change store name & path & comment the product

---
+++ src/Store/Store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import productSlice from './ProductReducer';
+import userDetailsSlice from './AuthReducer'
+
+export const store = configureStore({
+    reducer: {
+        // product: productSlice,
+        user: userDetailsSlice
+    }
+});
